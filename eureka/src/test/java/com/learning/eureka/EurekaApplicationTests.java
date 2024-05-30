863d04a013fa72aee9a2841e00e891a4513b17a0
tezoo2002 <tezoo2002@live.com>
2024-05-30 10:04:02 +0530

    Initial commit

---
+++ eureka/src/test/java/com/learning/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
